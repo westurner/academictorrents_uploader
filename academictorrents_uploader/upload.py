57c28a896e8a3cd38e59c88effd0a1ae526bfc2d
Wes Turner <wes@wrd.nu>
2016-11-13 11:04:06 -0600

    REF: upload.py: wrap in main(), return nonzero on error

--- academictorrents_uploader/upload.py
+++ academictorrents_uploader/upload.py
@@ -13,58 +13,65 @@ else:
     from urllib2 import Request, urlopen, HTTPError
 import re
 
-if len(sys.argv)!=9:
-    print("Usage: ./upload.py <api_key> <filename_or_directory> <name> <authors> <descr> <category> <tags> <urllist>")
-    print("Put api_key in quotes!")
-    sys.exit()
 
-# create the torrent
-torrent_args = sys.argv[0:2]
-torrent_args[1] = sys.argv[2]
-torrent_args.append('http://academictorrents.com/announce.php')
-torrentname = basename(sys.argv[2]) + ".torrent"
+def main():
+    if len(sys.argv)!=9:
+        print("Usage: ./upload.py <api_key> <filename_or_directory> <name> <authors> <descr> <category> <tags> <urllist>")
+        print("Put api_key in quotes!")
+        sys.exit()
 
-# if file exists, we assume torrent already created
-if not exists(torrentname):
-    maketorrent(torrent_args)
+    # create the torrent
+    torrent_args = sys.argv[0:2]
+    torrent_args[1] = sys.argv[2]
+    torrent_args.append('http://academictorrents.com/announce.php')
+    torrentname = basename(sys.argv[2]) + ".torrent"
 
-# get base64 of torrent
-f = open(torrentname, 'rb')
-b64_torrent = b64encode(f.read())
-f.close()
+    # if file exists, we assume torrent already created
+    if not exists(torrentname):
+        maketorrent(torrent_args)
 
-# extract cookie args
-matches = re.match("uid=(.*);pass=(.*)", sys.argv[1])
+    # get base64 of torrent
+    f = open(torrentname, 'rb')
+    b64_torrent = b64encode(f.read())
+    f.close()
 
-if sys.argv[6].lower() == 'dataset':
-    category = 6
-elif sys.argv[6].lower() == 'paper':
-    category = 5
-else:
-    print("Category must be either 'paper' or 'dataset'")
-    exit()
+    # extract cookie args
+    matches = re.match("uid=(.*);pass=(.*)", sys.argv[1])
 
-post_params = {
-    'uid' : matches.groups()[0],
-    'pass' : matches.groups()[1],
-    'name' : sys.argv[3],
-    'authors' : sys.argv[4],
-    'descr' : sys.argv[5],
-    'category' : category,
-    'tags' : sys.argv[7],
-    'urllist' : sys.argv[8],
-    'file' : b64_torrent
-}
+    if sys.argv[6].lower() == 'dataset':
+        category = 6
+    elif sys.argv[6].lower() == 'paper':
+        category = 5
+    else:
+        print("Category must be either 'paper' or 'dataset'")
+        exit()
 
-data = urlencode(post_params).encode('utf-8')
+    post_params = {
+        'uid' : matches.groups()[0],
+        'pass' : matches.groups()[1],
+        'name' : sys.argv[3],
+        'authors' : sys.argv[4],
+        'descr' : sys.argv[5],
+        'category' : category,
+        'tags' : sys.argv[7],
+        'urllist' : sys.argv[8],
+        'file' : b64_torrent
+    }
 
-req = Request('http://academictorrents.com/api/paper', data)
+    data = urlencode(post_params).encode('utf-8')
 
-try:
-    response = urlopen(req)
-except HTTPError as e:
-    print(e)
-    response = e
+    req = Request('http://academictorrents.com/api/paper', data)
 
-print(response.read())
+    try:
+        response = urlopen(req)
+    except HTTPError as e:
+        print(e)
+        response = e
+        print(response.read())
+        return 2
 
+    print(response.read())
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
